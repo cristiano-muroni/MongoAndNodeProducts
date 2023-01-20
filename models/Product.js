145b113820b8cd7f629e7ed3f0506cb2a2f98a37
Cristiano Muroni <muronislick@hotmail.com>
2023-01-19 21:44:08 -0300

    Desenvolvendo funções de pegar um produto e pegar todos os produtos

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,10 @@
 const conn = require('../db/conn');
+const { ObjectId } = require('mongodb');
 
 class Product {
-    constructor(name, price, description){
+    constructor(name, image, price, description){
         this.name = name
+        this.image = image
         this.price = price
         this.description = description
     };
@@ -10,6 +12,7 @@ class Product {
     save() {
         const product = conn.db().collection('products').insertOne({
             name: this.name,
+            image: this.image,
             price: this.price,
             description: this.description
         }) 
@@ -20,7 +23,11 @@ class Product {
         const products = conn.db().collection('products').find().toArray();
         return products;
     };
-};
 
+    static async getProductById(id) {
+        const product = await conn.db().collection('products').findOne({_id: ObjectId(id)});
+        return product;
+    };
+};
 module.exports = Product;
 
