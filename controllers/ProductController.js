cda184297aa8ce2c8569d926e372a6d2cf4ede1c
Cristiano Muroni <muronislick@hotmail.com>
2023-01-20 21:24:27 -0300

    criando editar

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -31,5 +31,11 @@ module.exports = class ProductController {
         const id = req.params.id;
         await Product.removeProductById(id);
         res.redirect('/products');
+    };
+
+    static async editProduct(req, res) {
+        const id = req.params.id;
+        const product = await Product.getProductById(id);
+        res.render('products/edit', { product });
     }
 };
